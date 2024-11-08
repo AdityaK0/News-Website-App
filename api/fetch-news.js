cd1221542a70cf1601bc4d328c03058e86247e98
Aditya Chaudhary <aditya.chaudhary@techseria.com>
2024-11-08 14:31:51 +0530

    Api Error

--- api/fetch-news.js
+++ api/fetch-news.js
@@ -1,22 +1,19 @@
 import fetch from 'node-fetch';
 
-const API_KEY = "a870c52cd08d4a75bd3119d39433d18a"; 
-
 export default async (req, res) => {
-  const { query } = req.query; 
+    const { query } = req.query;
+    if (!query) {
+        return res.status(400).json({ error: 'Missing query parameter' });
+    }
 
-  if (!query) {
-    return res.status(400).json({ error: 'Missing query parameter' });
-  }
-
-  try {
-    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&apikey=${API_KEY}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Failed to fetch news' });
-  }
+    try {
+        const API_KEY = process.env.NEWS_API_KEY; 
+        const url = `https://newsapi.org/v2/everything?q=${query}&apikey=${API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Failed to fetch news' });
+    }
 };
